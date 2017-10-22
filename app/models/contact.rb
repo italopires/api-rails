6da298af8ade4ca0fdb38154d27c1671693b0a9e
Italo Pires <italonapalm@gmail.com>
2017-10-21 23:28:38 -0300

    using include at as_json method in model to show association in json

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,10 +2,17 @@ class Contact < ApplicationRecord
   belongs_to :kind
 
   def as_json(options={})
-    super(methods: :author, root: true)
+    super(methods: [:author, :kind_description],
+          root: true,
+          include: { kind: { only: :description } }
+    )
   end
 
   def author
     "Ítalo Pires"
+  end
+
+  def kind_description
+    self.kind.description
   end
 end
