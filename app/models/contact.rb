7b0883c3f7c146097d2cf7d8b81084300cb19365
Italo Pires <italonapalm@gmail.com>
2017-10-22 00:16:05 -0300

    i18n rails implemented

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,18 +1,26 @@
 class Contact < ApplicationRecord
-  belongs_to :kind
+  belongs_to :kind, optional: true
 
-  def as_json(options={})
-    super(methods: [:author, :kind_description],
-          root: true,
-          include: { kind: { only: :description } }
-    )
+  # def as_json(options={})
+  #   super(methods: [:author, :kind_description],
+  #         root: true,
+  #         include: { kind: { only: :description } }
+  #   )
+  # end
+
+  # def author
+  #   "Ítalo Pires"
+  # end
+
+  # def kind_description
+  #   self.kind.description
+  # end
+
+  def hello
+    I18n.t 'hello'
   end
 
-  def author
-    "Ítalo Pires"
-  end
-
-  def kind_description
-    self.kind.description
+  def i18n
+    I18n.default_locale
   end
 end
